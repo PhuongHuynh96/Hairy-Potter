bf1368e3e1b3e3fd50ad25fff2a3a68aba834621
Aingty Eung <aingtyeung@yahoo.com>
2017-10-16 00:25:45 -0700

    Added Aingty's Code

---
+++ Aingty-Test/src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Welcome to the Game!");
+        Frame startGame = new Frame();
+  /*      startGame.setExtendedState(Frame.MAXIMIZED_BOTH);
+        startGame.setUndecorated(true);
+        startGame.setVisible(true);
+        */
+    }
+}

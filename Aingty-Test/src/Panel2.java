af4a9bfd220793822ab4d85a9117ef639356d2ed
Aingty Eung <aingtyeung@yahoo.com>
2017-10-16 22:29:41 -0700

    Fixed Your CODE!!!! I have completed this assignment! Good Luck

--- Aingty-Test/src/Panel2.java
+++ Aingty-Test/src/Panel2.java
@@ -12,45 +12,30 @@ import java.util.*;
  * @author Phuong Huynh
  *
  */
-public class Panel2 extends JPanel implements ActionListener, Runnable {
+public class Panel2 extends JPanel implements Runnable {
 	private int countDown;
 	String[] data;
 
 	private BufferedImageOp img;
     private JLabel map;
-	private JLabel gary;
-	private JLabel patrick;
-	private JLabel pearl;
-	private JLabel m;
+	private Player gary, patrick, pearl;
 	private JButton Button[] = new JButton[4];
-	private JList<String> options;
+	private JList options;
+	private DefaultListModel model = new DefaultListModel();
 	private MouseListener mouseListener;
-
+	private int counter = 1;
 	private static String option1;
+
+
 	public Panel2() {
 
 		this.setBackground(Color.GREEN);
 		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		
-		gary = new JLabel("Gary");
-        gary.setForeground(Color.BLUE);
-        gary.setFont(new Font("Serif", Font.BOLD, 40) );
-        gary.setBounds(610, 930, 100, 100);
-        
-        patrick = new JLabel("Patrick");
-        patrick.setForeground(Color.PINK);
-        patrick.setFont(new Font("Serif", Font.BOLD, 40) );
-        patrick.setBounds(610, 1000, 200, 200);
-        
-        pearl = new JLabel("Pearl");
-        pearl.setForeground(Color.DARK_GRAY);
-        pearl.setFont(new Font("Serif", Font.BOLD, 40) );
-        pearl.setBounds(610, 980, 100, 100);
-       
-        m=new JLabel("Marker");
-        m.setForeground(Color.CYAN);
-        m.setFont(new Font("Serif", Font.BOLD, 40));
-        m.setBounds(610, 950, 200, 200);
+		gary = new Player("Gary");
+		patrick = new Player("Patrick");
+		pearl = new Player("Pearl");
+		
 
         
         ImageIcon icon = new ImageIcon("../Map.png");
@@ -62,52 +47,31 @@ public class Panel2 extends JPanel implements ActionListener, Runnable {
 		this.add(Box.createRigidArea(new Dimension(0,5)));
 		this.add(jsp);
 
-        map.add(gary);
-        map.add(patrick);
-        map.add(pearl);
-        map.add(m);
-        //map.revalidate();    
+        map.add(gary.getPlayer());
+        map.add(patrick.getPlayer());
+        map.add(pearl.getPlayer());
+        map.revalidate();    
 
 
 		JPanel control = new JPanel();
 		control.setLayout(new BoxLayout(control, BoxLayout.LINE_AXIS));
-		//Hashmap of location combinations 
-		Map<String, ArrayList<String>> location = new HashMap<String, ArrayList<String>>();
-		location.put("ECS 308", new ArrayList<String>(Arrays.asList("South Hall")));
-		location.put("South Hall", new ArrayList<String>(Arrays.asList("ECS 302","EAT Club","CECS Conference Room","East Walkway")));
-		location.put("Lactation Lounge", new ArrayList<String>(Arrays.asList("South Hall")));
-		location.put("CECS Conference Room", new ArrayList<String>(Arrays.asList("South Hall")));
-		location.put("EAT Club", new ArrayList<String>(Arrays.asList("South Hall")));
-		location.put("Elevators", new ArrayList<String>(Arrays.asList("North Hall","LA 5")));
-		location.put("ECS 302", new ArrayList<String>(Arrays.asList("North Hall","South Hall")));
-		location.put("Computer Lab", new ArrayList<String>(Arrays.asList("North Hall")));
-		location.put("Room of Retirement", new ArrayList<String>(Arrays.asList("North Hall")));
-		location.put("North Hall", new ArrayList<String>(Arrays.asList("South Hall", "Room of Retirement","Elevators","ECS 302","Computer Lab","West Walkway")));
-		location.put("West Walkway", new ArrayList<String>(Arrays.asList("Library, North Hall, Rec Center")));
-		location.put("Library", new ArrayList<String>(Arrays.asList("West Walkway")));
-		location.put("LA 5", new ArrayList<String>(Arrays.asList("Elevators")));
-		location.put("Bratwurst Hall", new ArrayList<String>(Arrays.asList("East Walkway")));
-		location.put("East Walkway", new ArrayList<String>(Arrays.asList("South Hall","Bratwurst Hall", "Forbidden Parking")));
-		location.put("Forbidden Parking", new ArrayList<String>(Arrays.asList("East Walkway")));
-		location.put("Rec Center", new ArrayList<String>(Arrays.asList("West Walkway")));
 
-		data = new String[] { "South Hall", "North Hall" };
-		options = new JList<String>(data);
-		mouseListener = new MouseAdapter() {
-			public void mouseClicked(MouseEvent mouseEvent) {
-			  JList theList = (JList) mouseEvent.getSource();
-			  if (mouseEvent.getClickCount() == 1) {
-				int index = theList.locationToIndex(mouseEvent.getPoint());
-				if (index >= 0) {
-				   Object o = theList.getModel().getElementAt(index);
-				   option1 = o.toString();
-				  //System.out.println("Double-clicked on: " + o.toString());
-				}
-			  }
-			}
-		  };
-		  options.addMouseListener(mouseListener);
-		  System.out.println(option1);
+		options = new JList(model);
+		// mouseListener = new MouseAdapter() {
+		// 	public void mouseClicked(MouseEvent mouseEvent) {
+		// 	  JList theList = (JList) mouseEvent.getSource();
+		// 	  if (mouseEvent.getClickCount() == 1) {
+		// 		int index = theList.locationToIndex(mouseEvent.getPoint());
+		// 		if (index >= 0) {
+		// 			Object o = theList.getModel().getElementAt(index);
+		// 		 	System.out.println("Double-clicked on: " + o.toString());
+		// 		}
+		// 	  }
+		// 	}
+		//   };
+		//options.addMouseListener(mouseListener);
+
+		ButtonListener bl = new ButtonListener();
 
 		// making button arrays
 		Button[0] = new JButton("Draw Card");
@@ -116,6 +80,7 @@ public class Panel2 extends JPanel implements ActionListener, Runnable {
 
 		Button[1] = new JButton("Move");
 		Button[1].setVisible(true);
+		Button[1].addActionListener(bl);
 
 		Button[2] = new JButton("Play Card");
 		Button[2].setVisible(true);
@@ -127,9 +92,9 @@ public class Panel2 extends JPanel implements ActionListener, Runnable {
 
 
 		// Allowing user to click on button
-		Button[0].addActionListener(this);
-		Button[1].addActionListener(this);
-		Button[2].addActionListener(this);
+		//Button[0].addActionListener(this);
+		//Button[1].addActionListener(this);
+		//Button[2].addActionListener(this);
 
 		control.add(Button[0]);
 		control.add(Button[1]);
@@ -142,13 +107,18 @@ public class Panel2 extends JPanel implements ActionListener, Runnable {
 		this.add(Box.createRigidArea(new Dimension(0,200)));
 
 
-
 	}
 
 	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		
-		
+		if (counter == 1)
+		{
+			super.paintComponent(g);
+			for (String item: gary.getPossibleLocation())
+			{
+				model.addElement(item);
+			}
+		}
+		counter = 0;	
 	}
 
 	
@@ -166,6 +136,7 @@ public class Panel2 extends JPanel implements ActionListener, Runnable {
 			try {
 				Thread.sleep(50);
 				repaint();
+				
 			} catch (InterruptedException e) {
 				System.out.println("Error in the label reading.");
 			}
@@ -173,10 +144,23 @@ public class Panel2 extends JPanel implements ActionListener, Runnable {
 
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-
+	public class ButtonListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			if (e.getSource() == Button[1])
+			{
+				gary.setLocation(options.getSelectedValue().toString());
+				int rand1 = 0 + (int)(Math.random() * patrick.getPossibleLocation().length - 1);
+				int rand2 = 0 + (int)(Math.random() * pearl.getPossibleLocation().length - 1); 
+				patrick.setLocation(patrick.getPossibleLocation()[rand1]);
+				pearl.setLocation(pearl.getPossibleLocation()[rand2]);
+				gary.movePlayer();
+				model.removeAllElements();
+				counter = 1;
+				
+			}
+		}
 	}
 
 }
--- Aingty-Test/src/Player.java
+++ Aingty-Test/src/Player.java
@@ -4,13 +4,87 @@ import java.awt.image.*;
 import javax.swing.*;
 import java.util.*;
 
-public class Player
-{
+public class Player {
+	
+    private JLabel character;
     private String name;
-    private Location location;
+    private String locale;
 
-    public Player()
+    //Hashmap of location combinations 
+    Map<String, String[]> location = new HashMap<String, String[]>();
+    Map<String, Point[] > updateLocation = new HashMap<String, Point[]>();
+
+	
+	public Player(String x){
+        character = new JLabel(x);
+        locale = "ECS 308";
+		if (x.equalsIgnoreCase("Gary")){
+            name = x;
+            character.setForeground(Color.BLUE);
+            character.setFont(new Font("Serif", Font.BOLD, 40) );
+            character.setBounds(610, 930, 100, 100);
+		}
+		else if (x.equalsIgnoreCase("Patrick")){
+            name=x;
+			 character.setForeground(Color.PINK);
+		     character.setFont(new Font("Serif", Font.BOLD, 40) );
+		     character.setBounds(610, 1000, 200, 200);
+		}
+		else if(x.equalsIgnoreCase("Pearl")){
+            name=x;
+	        character.setForeground(Color.DARK_GRAY);
+	        character.setFont(new Font("Serif", Font.BOLD, 40) );
+	        character.setBounds(610, 980, 100, 100);
+        }
+        
+        location.put("ECS 308", new String[] {"South Hall"});
+		location.put("South Hall", new String[] {"ECS 302","EAT Club","CECS Conference Room","East Walkway"});
+		location.put("Lactation Lounge", new String[] {"South Hall"});
+		location.put("CECS Conference Room", new String[] {"South Hall"});
+		location.put("EAT Club", new String[] {"South Hall"});
+		location.put("Elevators", new String[] {"North Hall","LA 5"});
+		location.put("ECS 302", new String[] {"North Hall","South Hall"});
+		location.put("Computer Lab", new String[] {"North Hall"});
+		location.put("Room of Retirement", new String[] {"North Hall"});
+		location.put("North Hall", new String[] {"South Hall", "Room of Retirement","Elevators","ECS 302","Computer Lab","West Walkway"});
+		location.put("West Walkway", new String[] {"Library, North Hall, Rec Center"});
+		location.put("Library", new String[] {"West Walkway"});
+		location.put("LA 5", new String[] {"Elevators"});
+		location.put("Bratwurst Hall", new String[] {"East Walkway"});
+		location.put("East Walkway", new String[] {"South Hall","Bratwurst Hall", "Forbidden Parking"});
+		location.put("Forbidden Parking", new String[] {"East Walkway"});
+        location.put("Rec Center", new String[] {"West Walkway"});
+
+        updateLocation.put("South Hall", new Point[] {new Point(100,250), new Point(500,1200), new Point(800,100)});
+        
+	}
+    
+    public JLabel getPlayer()
     {
-
+        return this.character;
     }
+
+    public void setLocation(String x)
+    {
+        this.locale = x;
+    }
+    
+    public String[] getPossibleLocation()
+    {
+        return location.get(this.locale);
+    }
+
+    public String getLocation()
+    {
+        return this.locale;
+    }
+
+    public void movePlayer(){
+        if(name.equalsIgnoreCase("gary")){
+            int x = (int) updateLocation.get(this.locale)[0].getX();
+            int y = (int) updateLocation.get(this.locale)[0].getY();
+            character.setBounds(x,y,100,100);
+        }
+    }
+
 }
